d397fc145af2819866e54fb946e7ba3a14dc11c4
Matheus Manzutti <mateusmanzutti@gmail.com>
2021-12-27 22:20:29 -0300

    Criando a classe Factory

---
+++ src/tests/factory/AccountFactory.java
@@ -0,0 +1,19 @@
+package tests.factory;
+
+import entities.Account;
+
+//Padrão de projeto "Fábrica" que é que tem que ter uma classe para instanciar os objetos para mim,
+//se for uma operação repetitiva no sistema. 
+
+public class AccountFactory {
+
+	//método auxiliar que sempre irá criar um conta com saldo zero
+	public static Account createEmptyAccount() {
+		return new Account(1L, 0.0);
+	}
+	
+	//método para informar um certo saldo que quer informar
+	public static Account createAccount(double initialBalance) { //initialBalance = saldo inicial da conta
+		return new Account(1L, initialBalance);
+	}
+}
